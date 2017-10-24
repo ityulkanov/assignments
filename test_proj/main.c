551a572512fce62d988c24030f08896341fb0b47
Igor T <ityulkanov@gmail.com>
2017-10-24 18:03:41 +0300

    Initial Commit

---
+++ test_proj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test_proj
+//
+//  Created by Igor T on 10/24/17.
+//  Copyright © 2017 appmasters. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
